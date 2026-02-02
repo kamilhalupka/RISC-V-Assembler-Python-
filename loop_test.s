065c508c90d3433dda487fc617178c3434de44dd
Kamil Halupka <kamilhalupka11@gmail.com>
2026-02-02 09:26:32 -0600

    Stopped tracking files

--- loop_test.s
+++
@@ -1,8 +0,0 @@
-addi x1, x0, 0
-addi x2, x0, 5
-LOOP:
-beq x1, x2, END
-addi x1, x1, 1
-jal x0, LOOP
-END:
-add x1, x0, x0
--- memory_ops.s
+++
@@ -1,6 +0,0 @@
-addi x10, x0, 1024
-addi x5, x0, 255
-sw x5, 0(x10)
-sw x5, 4(x10)
-lw x6, 0(x10)
-lw x7, 4(x10)
